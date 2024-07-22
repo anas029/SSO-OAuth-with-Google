20f3958575f36536139f998b6975601521ea1c23
alinaser <aliyaqoob+reboot@gmail.com>
2024-07-22 21:36:18 +0300

    fix: redirection and scope

--- routes/api/v1/google.js
+++ routes/api/v1/google.js
@@ -1,15 +1,14 @@
 import { Router } from "express";
 import passport from "../../../auth/Google.js";
-import { DEBUG, successRedirect } from "../../../configs/env.js";
+import { successRedirect, failureRedirect } from "../../../configs/env.js";
 
 const router = Router()
 
 // API Endpoint: /api/v1/auth/google
-router.get("/login", passport.authenticate("google", { accessType: 'offline' }))
+router.get("/login", passport.authenticate("google", { scope: ['profile', 'email'], accessType: 'offline' }))
 // router to handle response for google after sign-in
 router.get("/callback", passport.authenticate("google", {
-    failureMessage: "Error",
-    failureRedirect: "/falied",
+    failureRedirect,
     successRedirect
 }))
 // Route to logout user 

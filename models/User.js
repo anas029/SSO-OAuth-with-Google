94b11d9f411b1cf3fe29a53b98e0c913512e25be
alinaser <aliyaqoob+reboot@gmail.com>
2024-07-22 21:32:06 +0300

    add user roles

--- models/User.js
+++ models/User.js
@@ -1,10 +1,11 @@
 import mongoose, { Schema } from "mongoose";
-
+export const userRoles = ["Admin", "User"]
 const schema = new Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     photo: { type: String, required: true },
     googleId: { type: String, required: true, unique: true },
+    role: { type: String, required: true, default: "User", enum: userRoles },
     refreshToken: { type: String, required: false },
     iv: { type: String, required: false },
     lastCheck: { type: Date, required: false }
